ea874751754a68df4bd9ddb54c1ffa05a4d02c6a
Sebastian Jablonski <s.jablonski.684@studms.ug.edu.pl>
2024-03-26 14:35:46 +0100

    Druga czesc zadania z lab1 + refactor pierwszej czesci

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,32 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        System.out.println("Factorial of " + n + " is equal: " + result);
+
+    private static void displayMenu() {
+        System.out.println("Choose an option:");
+        System.out.println("1. Factorial");
+        System.out.println("2. Sum");
+        System.out.println("3. Exit");
+        System.out.println("What would you like to do? (Enter one of options)");
+    }
+
+    private static void factorialChoice() {
+        Scanner myObj = new Scanner((System.in));
+        System.out.println("Enter value of factorial you want to calculate: ");
+        int factorialN = myObj.nextInt();
+        int result = Calculations.factorial(factorialN);
+        System.out.println("Factorial of " + factorialN + " is equal: " + result);
+    }
+
+    private static void sumChoice() {
+        Scanner myObj = new Scanner((System.in));
+        System.out.println("Enter first value for sum.");
+        int a = myObj.nextInt();
+        System.out.println("Enter second value for sum.");
+        int b = myObj.nextInt();
+        int result = Calculations.sum(a, b);
+        System.out.println("Your sum from " + a + " to " + b + " is equal: " + result);
     }
 
     public static void main(String[] args) {
         boolean working = true;
         while (working) {
+            displayMenu();
             Scanner myObj = new Scanner((System.in));
-            System.out.println("Woudl you like to calculate factorial or exit program? (Options: Calculate, Exit)");
             String choice = myObj.nextLine();
             switch (choice) {
-                case "Calculate":
-                    System.out.println("Enter value of factorial you want to calculate: ");
-                    int factorialN = myObj.nextInt();
-                    factorial(factorialN);
-                    break;
-                case "Exit":
+                case "1" -> {
+                    factorialChoice();
+                }
+                case "2" -> {
+                    sumChoice();
+                }
+                case "3"-> {
                     System.out.println("End of program. See you soon!");
                     working = false;
-                    break;
-                default:
-                    System.out.println("Wrong option input!");
+                }
+                default -> System.out.println("Wrong option input!");
             }
         }
     }
